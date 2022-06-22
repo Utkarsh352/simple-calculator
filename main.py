a19aad91fe059f3c6ae6a7ebc7aa06af79f0c226
Utkarsh Mahajan <89238212+Utkarsh352@users.noreply.github.com>
2022-06-23 03:43:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ while True:
     else:
         print("Please enter a number")
         numA=float(input())
-        print("what would you like to do with it\n1=>add\n2=>subtract\n3=>multiply\n4=>divide")
+        print("what would you like to do with it\n1=>add\n2=>subtract\n3=>multiply\n4=>divide\n5=>exponentiation")
         operator=int(input())
         print("enter the 2nd number")
         numB=float(input())
-        if operator==1:
-            print(numA+numB)
-        elif operator==2:
-            print(numA-numB)
-        elif operator==3:
-            print(numA*numB)
-        elif operator==4:
-            print(numA/numB)
+        try:    
+            if operator==1:
+                print(numA+numB)
+            elif operator==2:
+                print(numA-numB)
+            elif operator==3:
+                print(numA*numB)
+            elif operator==4:
+                print(numA/numB)
+            elif operator==5:
+                print(numA**numB)
+         except:
+            print("oops chech the operator again")
+print("thanks for using this calculator")
